81c55cd73821eefa54fe8a3638496656fd4ec7ad
root <root@earth>
2023-09-20 00:07:16 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const mineflayer = require('mineflayer')
+
+const bot = mineflayer.createBot({
+  host: 'zxc_vanilla.aternos.me', // minecraft server ip
+  port: 43261,
+  username: 'Bot', // username or email, switch if you want to change accounts
+  auth: 'offline' // for offline mode servers, you can set this to 'offline'
+})
+
+
